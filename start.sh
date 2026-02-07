3bf8adb236c5476c1c23832a38c010f9b1200ba5
INikitosI <121294564+INikitosI@users.noreply.github.com>
2026-02-07 22:10:18 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# start.sh
 
+# Делаем скрипт исполняемым (на Render это делается автоматически, но лучше явно)
+chmod +x start.sh
+
+# Запускаем основной скрипт Python
+python my_bot.py
